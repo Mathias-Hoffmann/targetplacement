9c44cb8fd5ade59658c87d1dbee67c69c5c018f2
Mathias-Hoffmann <mathias.hoffmann@imt-atlantique.net>
2025-12-19 15:11:56 +0100

    Add Vite app + GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+>>>>>>> 6bbee61 (Ajout de la simulation 3D commentée)
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 
@@ -9,4 +10,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/simulidar/", 
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+>>>>>>> 6bbee61 (Ajout de la simulation 3D commentée)
 })
